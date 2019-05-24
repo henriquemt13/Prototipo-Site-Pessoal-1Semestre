156c205feadddc5365a880d78f311a293efaa96d
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-24 16:50:32 -0300

    adicionando site

---
+++ banco.sql
@@ -0,0 +1,15 @@
+create database AA;
+use AA;
+
+create table Cadastrar(
+id int primary key identity(1,1),
+nome varchar(60),
+telefone varchar(14),
+email varchar (70),
+senha varchar(30),
+endereco varchar(70),
+cidade varchar(60),
+estado varchar(2),
+cep varchar(9)
+);
+
