d1476e0dc717785ca5ba6436cb1459c15ca689ab
Henriqe Matos <henriquee.matos@hotmail.com>
2019-05-25 14:04:55 -0300

    nodemon

--- site/routes/usuarios.js
+++ site/routes/usuarios.js
@@ -9,7 +9,7 @@ router.post('/entrar', function (req, res, next) {
   banco.conectar().then(() => {
     var login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
     var senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
-    return banco.sql.query(`select * from cadastrar where login='${login}' and senha='${senha}'`);
+    return banco.sql.query(`select * from cadastrar where email='${login}' and senha='${senha}'`);
   }).then(consulta => {
 
     console.log(`Usuários encontrados: ${consulta.recordset}`);
